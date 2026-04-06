4aafb1dad0c0e53b37096ae115bf77f86b5faa73
garimabajaj07 <garima.bajaj126@gmail.com>
2026-04-06 18:54:09 +0530

    in progress

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -8,7 +8,7 @@ const UserRouting= Router()
 UserRouting.post("/submit", submit)
 UserRouting.get("/records", checkToken,fetchRecords)
 UserRouting.post("/login", login)
-UserRouting.get("/check", checkToken, (req, res) => {
+UserRouting.get("/checklogin", checkToken, (req, res) => {
     res.status(200).json({ message: "User Authenticated" })
 })
 UserRouting.delete("/deleteuser/:id",checkToken, deleteUser)
