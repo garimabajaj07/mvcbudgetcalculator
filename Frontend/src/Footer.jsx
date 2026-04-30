219d5b24dfabf6b3bff5e4b6be93310bd008e8ca
rohitjainfsl <rohit@fullstacklearning.com>
2026-04-30 18:06:14 +0530

    created footer

--- Frontend/src/Footer.jsx
+++ Frontend/src/Footer.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div></div>
-  )
+    <footer className="footer">
+      <p>All rights reserved &copy; {new Date().getFullYear()}</p>
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="">Register as Seller</Link>
+        </li>
+      </ul>
+    </footer>
+  );
 }
